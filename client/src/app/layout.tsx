febc275e2526d9ef9c3912d46343c95063c67955
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2026-06-23 23:52:23 +0500

    cookies script added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
         <Script src="https://cdn.iubenda.com/iubenda.js" strategy="lazyOnload" />
         {/* <Script strategy="lazyOnload" src="https://www.iubenda.com/privacy-policy/40029799/cookie-policy" />
         <script type="text/javascript" src="https://embeds.iubenda.com/widgets/4150beb2-3cab-44a9-9fe6-fd5c93d6cbd6.js"></script> */}
+         <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/390ecbff1413870983f6e78b6331e66b/script.js"></script>
       </body>
     </html>
   );
